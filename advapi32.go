36ef43f482d6d4713b821149108c6a886d188122
Dusty Wilson <dusty@linux.com>
2012-05-05 04:33:39 -0700

    Added RegSetValueEx to allow writing to the registry

--- advapi32.go
+++ advapi32.go
@@ -21,9 +21,27 @@ const (
 	HKEY_DYN_DATA         HKEY = 0x80000006
 )
 
+const (
+	TYPE_REG_NONE                       REGTYPE = 0
+	TYPE_REG_SZ                         REGTYPE = 1
+	TYPE_REG_EXPAND_SZ                  REGTYPE = 2
+	TYPE_REG_BINARY                     REGTYPE = 3
+	TYPE_REG_DWORD                      REGTYPE = 4
+	TYPE_REG_DWORD_LITTLE_ENDIAN        REGTYPE = 4
+	TYPE_REG_DWORD_BIG_ENDIAN           REGTYPE = 5
+	TYPE_REG_LINK                       REGTYPE = 6
+	TYPE_REG_MULTI_SZ                   REGTYPE = 7
+	TYPE_REG_RESOURCE_LIST              REGTYPE = 8
+	TYPE_REG_FULL_RESOURCE_DESCRIPTOR   REGTYPE = 9
+	TYPE_REG_RESOURCE_REQUIREMENTS_LIST REGTYPE = 10
+	TYPE_REG_QWORD                      REGTYPE = 11
+	TYPE_REG_QWORD_LITTLE_ENDIAN        REGTYPE = 11
+)
+
 type (
 	ACCESS_MASK uint32
 	HKEY        HANDLE
+	REGTYPE     uint32
 	REGSAM      ACCESS_MASK
 )
 
@@ -35,6 +53,7 @@ var (
 	regCloseKey     uintptr
 	regOpenKeyEx    uintptr
 	regQueryValueEx uintptr
+	regSetValueEx   uintptr
 )
 
 func init() {
@@ -45,6 +64,7 @@ func init() {
 	regCloseKey = MustGetProcAddress(libadvapi32, "RegCloseKey")
 	regOpenKeyEx = MustGetProcAddress(libadvapi32, "RegOpenKeyExW")
 	regQueryValueEx = MustGetProcAddress(libadvapi32, "RegQueryValueExW")
+	regSetValueEx = MustGetProcAddress(libadvapi32, "RegSetValueExW")
 }
 
 func RegCloseKey(hKey HKEY) int32 {
@@ -76,6 +96,18 @@ func RegQueryValueEx(hKey HKEY, lpValueName *uint16, lpReserved, lpType *uint32,
 		uintptr(unsafe.Pointer(lpType)),
 		uintptr(unsafe.Pointer(lpData)),
 		uintptr(unsafe.Pointer(lpcbData)))
+
+	return int32(ret)
+}
+
+func RegSetValueEx(hKey HKEY, lpValueName *uint16, lpReserved, dwType *REGTYPE, lpData *byte, cbData *uint32) int32 {
+	ret, _, _ := syscall.Syscall6(regSetValueEx, 6,
+		uintptr(hKey),
+		uintptr(unsafe.Pointer(lpValueName)),
+		uintptr(unsafe.Pointer(lpReserved)),
+		uintptr(unsafe.Pointer(dwType)),
+		uintptr(unsafe.Pointer(lpData)),
+		uintptr(unsafe.Pointer(cbData)))
 
 	return int32(ret)
 }
